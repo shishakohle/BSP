b8ebb68f117917a1571ef9272308d0568f924a72
Ingo Weigel <me19m022@technikum-wien.at>
2020-10-04 15:09:12 +0200

    clean up repo and create a commit with celan diff

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -1,5 +1,6 @@
-clc;
 clear;
+close all;
+clc;
 
 %% Comment and uncomment according to your EDF file location
 
@@ -13,8 +14,8 @@ edfFilename  = "s1_high_resistance_bike.edf";
 % edfFilename  = "";
 
 % choose on the path notion of your operating system:
-% filepath = edfSubfolder + "\" + edfFilename; % use this line for MS Windows
-filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distributions / Mac OS X
+filepath = edfSubfolder + "\" + edfFilename; % use this line for MS Windows
+% filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distributions / Mac OS X
 
 %% Execute the essence function of Assignment 1
 
@@ -39,24 +40,33 @@ function features = extract_basic_features(filepath)
     plot(time, wrist_ppg);
     
     %% Filter and segment signal
+    
     % apply bandpass
     min_freq = 30/60; % minimum expectable frequency is 30 bpm -> converted to Hz
     max_freq = 210/60; % maximum expectable frequency is 210 bpm -> converted to Hz
-    fpass = [min_freq max_freq]; 
+    fpass = [min_freq max_freq]/(sample_freq*0.5); 
+    [b, a] = butter(2, fpass, 'bandpass');
+    wrist_ppg_filtered = filter(b, a, wrist_ppg);
     
-    wrist_ppg_filtered = bandpass(wrist_ppg, fpass, sample_freq);
-    
-    % plot filtered signal
+    % plot unfiltered and filtered signal
     figure;
-    time = ((1:size(record, 2))) / sample_freq;
+    hold on;
+    time = ((1:size(record, 2)))/sample_freq;
+    subplot(2, 1, 1);
+    plot(time, wrist_ppg);
+    title('Unfiltered');
+    subplot(2, 1, 2);
     plot(time, wrist_ppg_filtered);
+    title('BP filtered');
+    hold off;
     
     % segment into 60s windows
     datapoints_per60s = sample_freq*60;
     width_wrist_ppg_filtered = size(wrist_ppg_filtered, 2);
     rownumber = size((1 : datapoints_per60s : width_wrist_ppg_filtered), 2);
     length_lastrow = mod(width_wrist_ppg_filtered, datapoints_per60s);
-
+    
+    % matrix approach
     wrist_ppg_filtered_windowed = NaN(rownumber, datapoints_per60s); % preallocating matrix for 60 second windows with NaN values
     
     count = 1;
@@ -66,6 +76,21 @@ function features = extract_basic_features(filepath)
             break;
         end
         wrist_ppg_filtered_windowed(count, :) = wrist_ppg_filtered(1, i:(i-1+datapoints_per60s));
+        count = count+1;
+    end
+    
+    % cell array approach
+    wrist_ppg_filtered_windowed_2 = cell(rownumber, 1); % preallocating cell array for 60 second windows
+    
+    count = 1;
+    for i = 1 : datapoints_per60s : width_wrist_ppg_filtered
+        if (i-1+datapoints_per60s) > width_wrist_ppg_filtered
+            window_60s = wrist_ppg_filtered(1, i:end);
+            wrist_ppg_filtered_windowed_2(count, :) = {window_60s};
+            break;
+        end
+        window_60s = wrist_ppg_filtered(1, i:(i-1+datapoints_per60s));
+        wrist_ppg_filtered_windowed_2(count, :) = {window_60s};
         count = count+1;
     end
     
