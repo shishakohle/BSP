93463622fa1996aa65a3f3a81a0346a78c2a0d3d
Ingo Weigel <me19m022@technikum-wien.at>
2020-10-05 16:09:50 +0200

    Tranpose windowed signal. Use windowed signal for NaN(), mean() and var().

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -31,7 +31,7 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     % Read header and rawdata from the EDF file
     [hdr, record] = edfread(filepath);
     
-    wrist_ppg = record(2,:);
+    wrist_ppg   = record(2,:);
     sample_freq = hdr.frequency(2);
     
     % plot original signal
@@ -42,13 +42,14 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     %% Filter and segment signal
     
     % apply bandpass
-    min_freq = 30/60; % minimum expectable frequency is 30 bpm -> converted to Hz
+    min_freq =  30/60; % minimum expectable frequency is 30 bpm -> converted to Hz
     max_freq = 210/60; % maximum expectable frequency is 210 bpm -> converted to Hz
+    
+    % wrist_ppg_filtered = bandpass(wrist_ppg, [min_freq max_freq], sample_freq);
+    
     fpass = [min_freq max_freq]/(sample_freq*0.5); 
     [b, a] = butter(2, fpass, 'bandpass');
     wrist_ppg_filtered = filter(b, a, wrist_ppg);
-    
-    % wrist_ppg_filtered = bandpass(wrist_ppg, [min_freq max_freq], sample_freq);
     
     % plot unfiltered and filtered signal
     figure;
@@ -63,18 +64,18 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     hold off;
     
     % segment into 60s windows    
-    ppg_windows = buffer(wrist_ppg_filtered, sample_freq*60, 0);
+    ppg_windows = transpose( buffer(wrist_ppg_filtered, sample_freq*60, 0) );
     
     %% Extract features
-    features = NaN(rownumber, 8); % preallocating matrix for the features with NaN values - one row per time window and 8 columns for the extracted features
+    features = NaN( size(ppg_windows,1), 8 ); % preallocating matrix for the features with NaN values - one row for each time window and 8 columns for 8 extracted features each
     
-    % statistical features time domain
-    features(:, 1) = mean(wrist_ppg_filtered_windowed, 2, 'omitnan'); % mean of all rows, ignoring NaN values
-    features(:, 2) = var(wrist_ppg_filtered_windowed, 0, 2, 'omitnan'); % variance of all rows, with default weighting, ignoring NaN values
+    % statistical features time domain (Assignment 1.1)
+    features(:, 1) = mean(ppg_windows, 2,    'omitnan'); % mean of all rows, ignoring NaN values
+    features(:, 2) = var (ppg_windows, 0, 2, 'omitnan'); % variance of all rows, with default weighting, ignoring NaN values
     
-    % frequency domain features based on fourier transform
+    % frequency domain features based on fourier transform (Assignment 1.2)
         % use fft() function ?
     
-    % time domain features based on inter-beat intervals
+    % time domain features based on inter-beat intervals (Assignment 1.3)
     
 %end
