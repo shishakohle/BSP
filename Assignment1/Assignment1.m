657adf2297d16db9a7cacf4b64aa372b447df5c6
Ingo Weigel <me19m022@technikum-wien.at>
2020-10-10 13:15:06 +0200

    Calculate mean and median frequency in snippets. Get Assignment 1 ready for final hand-in.

--- Assignment1/Assignment1.m
+++ Assignment1/Assignment1.m
@@ -1,8 +1,14 @@
+%% Biosignal Processing - Assingment 1
+% Group H: Alexander Neumayer, Lukas Riedler, Laura Kummer, Ingo Weigel
+% 2020-10-10
+% Find this code in the project repository on GitHub:
+% https://github.com/shishakohle/BSP
+
 clear;
 close all;
 clc;
 
-%% Comment and uncomment according to your EDF file location
+%% Comment and uncomment according to your EDF file location and your OS
 
 % choose on an EDF subfolder:
 % edfSubfolder = ".";
@@ -10,25 +16,42 @@ edfSubfolder = "edf";
 
 % choose on an EDF file:
 edfFilename  = "s1_high_resistance_bike.edf";
-% edfFilename  = "";
+% edfFilename  = ""; % TODO add further files
 % edfFilename  = "";
 
 % choose on the path notion of your operating system:
-%filepath = edfSubfolder + "\" + edfFilename; % use this line for MS Windows
-filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distributions / Mac OS X
+% uncomment the next line for MS Windows
+% filepath = edfSubfolder + "\" + edfFilename;
+% uncomment the next line for Linux distributions / Mac OS X
+filepath = edfSubfolder + "/" + edfFilename;
+
+clear edfSubfolder;
+clear edfFilename;
 
 %% Execute the essence function of Assignment 1
 
-% features = extract_basic_features(filepath);
+features = extract_basic_features(filepath);
 
 
 %% Function extract_basic_features as a solution to Assignment 1
-% argument: TODO
-% return value(s): TODO
+% argument:
+%   Path to an EDF file containing a Wrist PPG signal in index 2.
+% return value(s):
+%   A matrix containing one line per 60 seconds in the PPG signal.
+%   Each column represents a certain feature of the corresponding
+%   60 seconds snippet:
+%       column 1: mean
+%       column 2: variance
+%       column 3: frequency with the maximum power
+%       column 4: median frequency
+%       column 5: mean frequency
+%       column 6: mean inter-beat interval
+%       column 7: variance of inter-beat intervals
+%       column 8: skewness of the distribution of inter-beat intervals
 
-% function features = extract_basic_features(filepath)
+function features = extract_basic_features(filepath)
 
-    % Read header and rawdata from the EDF file
+    %% Read header and rawdata from the EDF file
     [hdr, record] = edfread(filepath);
     
     wrist_ppg   = record(2,:);
@@ -88,16 +111,22 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     
     %% Extract features
     
-    % preallocating matrix for the features with NaN values - one row for each time window and 8 columns for 8 extracted features each
+    % The matrix features is to be used as the final return value in the
+    % end.
+    
+    % preallocating matrix for the features with NaN values - one row for
+    % each time window and 8 columns for 8 extracted features each
     features = NaN(window_count, 8);
     
     %% statistical features (time domain) (Assignment 1.1)
-    features(:, 1) = mean(wrist_ppg_filtered_windows, 2,    'omitnan'); % mean of all rows, ignoring NaN values
-    features(:, 2) = var (wrist_ppg_filtered_windows, 0, 2, 'omitnan'); % variance of all rows, with default weighting, ignoring NaN values
+    % mean of all rows, ignoring NaN values
+    features(:, 1) = mean(wrist_ppg_filtered_windows, 2,    'omitnan');
+    % variance of all rows, with default weighting, ignoring NaN values
+    features(:, 2) = var (wrist_ppg_filtered_windows, 0, 2, 'omitnan');
     
     %% frequency domain features based on fourier transform (Assignment 1.2)
     
-    % provisory choose a single signal
+    % prepare signal in such a way that it can be processed by fft() and others
     signal = transpose (wrist_ppg_filtered_windows);
     
     % calculate Power Spectral Density (PSD), following the hints in:
@@ -108,7 +137,7 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     % calculte DFT (i.e. the complex Fourier Coefficients)
     % by utilizing the FFT algorithm
     DFT = fft(signal);
-    % TODO remove NaNs from last window first!!
+    % TODO remove NaNs from last window first!! not done yet.
     
     % "Compute the power spectral density, a measurement of the energy at
     %  various frequencies, using the complex conjugate (CONJ)."
@@ -130,28 +159,38 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
     features(:,3) = f_max_a;
     
     %% find the median frequency (Assignment 1.2b)
-    % TODO
+    f_med = medfreq(wrist_ppg_filtered_windows(:,1:9),f_sample);
+    f_med(10) = medfreq(wrist_ppg_filtered_windows(~isnan(wrist_ppg_filtered_windows(:,10))),f_sample);
+    % TODO this is hard-coded for the signal having 10 windows, where the
+    % last window (no. 10) has NaNs to be removed from it.
     
     % add the median frequency to features
-    % TODO
+    features(:,4) = f_med;
     
     %% find the mean frequency (Assignment 1.2c)
-    % TODO
+    f_mean = meanfreq(wrist_ppg_filtered_windows(:,1:9),f_sample);
+    f_mean(10) = meanfreq(wrist_ppg_filtered_windows(~isnan(wrist_ppg_filtered_windows(:,10))),f_sample);
+    % TODO this is hard-coded for the signal having 10 windows, where the
+    % last window (no. 10) has NaNs to be removed from it.
     
     % add the mean frequency to features
-    % TODO
+    features(:,5) = f_mean;
     
     %% time domain features based on inter-beat intervals (Assignment 1.3)
-    min_RRinterval = 1/max_freq; %minimum possible time period between heartbeats [s]
+    
+    % minimum possible time period between heartbeats [s]
+    min_RRinterval = 1/max_freq;
 
-    peak_intervals = NaN(window_count, datapoints_per_window); % preallocating matrix for 60 second windows with NaN values
+    % preallocating matrix for 60 second windows with NaN values
+    peak_intervals = NaN(window_count, datapoints_per_window);
 
     for i = 1 : size(wrist_ppg_filtered_windows,1)
         signal = wrist_ppg_filtered_windows(i, :);
         time   = time( 1:length(signal) );
         [peak_vals, peak_locs, peak_widths, peak_prominences] = findpeaks(signal, time, 'MinPeakDistance', min_RRinterval);
         length_peak_widths = size(peak_widths, 2);
-        peak_intervals(i, 1:length_peak_widths) = peak_widths; %peak intervals [s]
+        % peak intervals [s]
+        peak_intervals(i, 1:length_peak_widths) = peak_widths;
         
         % optionally plot signal windows with peaks
 %         figure;
@@ -161,8 +200,9 @@ filepath = edfSubfolder + "/" + edfFilename; % use this line for Linux distribut
 %         ylabel('Amplitude [?]')
     end
     
+    % add features of the inter-beat intervalls to features
     features(:, 6) = mean(peak_intervals, 2, 'omitnan');
     features(:, 7) = var(peak_intervals, 0, 2, 'omitnan');
     features(:, 8) = skewness(peak_intervals, 0, 2);
     
-%end
+end
