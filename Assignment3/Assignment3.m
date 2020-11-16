90ec3c23dbf054f7303bf3924d1efbd9d33b17ac
LXdaMan <72304070+LXdaMan@users.noreply.github.com>
2020-11-16 17:21:27 +0100

    Start A3

---
+++ Assignment3/Assignment3.m
@@ -0,0 +1,9 @@
+%% Biosignal Processing - Assingment 2
+% Group H: Alexander Neumayer, Ingo Weigel
+% 2020-11-08
+% Find this code in the project repository on GitHub:
+% https://github.com/shishakohle/BSP
+
+clear;
+close all;
+clc;
---
+++ Assignment3/Validate.m
@@ -0,0 +1,6 @@
+%% Function Validate() as a solution to Assignment 4
+
+% read ECG data - ToDo Alex
+function [ECGtimes, ECGintervals] = readECG()
+
+end
