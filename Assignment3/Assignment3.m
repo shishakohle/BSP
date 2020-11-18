1d2e0642b0734490b2afeef1db4636d21b1829b1
LXdaMan <72304070+LXdaMan@users.noreply.github.com>
2020-11-18 17:37:22 +0100

    Setup data for validation
    added readPPG() to get PPG times and intervals / structured work packages a bit

--- Assignment3/Assignment3.m
+++ Assignment3/Assignment3.m
@@ -1,12 +1,46 @@
-%% Biosignal Processing - Assingment 2
+%% REMINDER
+% @Alex - don't forget to change "hard coding" of A2 - so that template is
+% created for the chosen EDF file
+
+%% Biosignal Processing - Assingment 3
 % Group H: Alexander Neumayer, Ingo Weigel
-% 2020-11-08
+% 2020-11-20
 % Find this code in the project repository on GitHub:
 % https://github.com/shishakohle/BSP
 
 clear;
 close all;
 clc;
+
+%% Comment and uncomment according to your EDF file location and your OS
+
+% choose on an EDF subfolder:
+% edfSubfolder = ".";
+edfSubfolder = "edf";
+
+% choose on an EDF file:
+edfFilename  = "s1_high_resistance_bike.edf";
+% edfFilename  = ""; % TODO add further files
+% edfFilename  = "";
+
+% choose on the path notion of your operating system:
+% uncomment the next line for MS Windows
+% filepath = edfSubfolder + "\" + edfFilename;
+% uncomment the next line for Linux distributions / Mac OS X
+filepath = edfSubfolder + "/" + edfFilename;
+
+clear edfSubfolder;
+clear edfFilename;
+
+%% Extract raw wrist PPG signal from EDF file
+
+[hdr, rawPPGsignal] = edfread(filepath, 'targetSignals', 'wrist_ppg');
+samplingRate        = hdr.samples / hdr.duration;
+
+%% Execute the essence function of Assignment 2
+
+[beatTimesAmplitudesPQIs, ~] = PQI(rawPPGsignal, ...
+    samplingRate);
 
 %% Choose your ECG .txt file via GUI
 [file, path] = uigetfile({'*.txt;'}, 'MultiSelect', 'on');
@@ -19,11 +53,28 @@ clear file path filepath;
 sampleInt = 256;
 locFirstbeat = 3;
 
-[ECGtimes, ECGintervals] = readECG(ECGdata, sampleInt); % time in seconds after start
+% get ECGtimes and ECGintervals
+[ECGtimes, ECGintervals] = readECG(ECGdata, sampleInt, locFirstbeat); % time in seconds after start and intervals in seconds
 
-function [ECGtimes, ECGintervals] = readECG(ECGdata, sampleInt)
+% get PPGtimes and PPGintervals
+[PPGtimes, PPGintervals] = readPPG(beatTimesAmplitudesPQIs); % time in seconds after start and intervals in seconds
 
-    ECGtimes = cell2mat(ECGdata(3:end, 2)) ./ sampleInt;
+% clear everything we don't need here anymore
+clear ECGdata hdr locFirstbeat rawPPGsignal sampleInt samplingRate;
+
+%% Call validate function
+
+%% Function declaration
+function [ECGtimes, ECGintervals] = readECG(ECGdata, sampleInt, locFirstbeat)
+
+    ECGtimes = cell2mat(ECGdata(locFirstbeat:end, 2)) ./ sampleInt;
     ECGintervals = diff(ECGtimes);
+
+end
+
+function [PPGtimes, PPGintervals] = readPPG(PPGdata)
+
+    PPGtimes = transpose(PPGdata(1, :));
+    PPGintervals = diff(PPGtimes);
 
 end
--- Assignment3/Validate.m
+++ Assignment3/Validate.m
@@ -1,1 +1,5 @@
-%% Function Validate() as a solution to Assignment 4
+%% Function Validate() as a solution to Assignment 3
+
+% validate beat detection - ToDo Ingo
+
+% validate inter beat interval durations - ToDo Alex
